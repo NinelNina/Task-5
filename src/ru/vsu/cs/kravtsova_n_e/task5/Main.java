df005000ce98e483d315a5deac583fd5b37d6c64
NinelNina <ninkr@list.ru>
2021-10-20 23:41:09 +0300

    Update Main.java
    Fix logic

--- src/ru/vsu/cs/kravtsova_n_e/task5/Main.java
+++ src/ru/vsu/cs/kravtsova_n_e/task5/Main.java
@@ -8,12 +8,14 @@ public class Main {
     }
 
     public static void printPicture(int h, int w) {
-        int symbInLastRow = 1;
+        int symbInLastRow = 0;
         int countOfSpaces = 0;
         int countOfSymb = 0;
+        int numberOfSymb = 1;
+        int symbLeft = 0;
+        boolean isOnNewRow = false;
 
         for (int i = 0; i < h; i++) {
-            symbInLastRow += i-1;
             for (int j = 0; j < w; j++) {
                 if (i == 0 || i == (h - 1)) {
                     if (j == 0 || j == (w - 1)) {
@@ -25,19 +27,44 @@ public class Main {
                     if (j == 0 || j == (w - 1)) {
                         System.out.print("|");
                     } else {
-                        if (countOfSpaces != symbInLastRow) {
-                            System.out.print(" ");
-                            countOfSpaces++;
-                        } else if (countOfSymb != i) {
-                            System.out.print("!");
-                            countOfSymb++;
-                        } else {
-                            System.out.print(" ");
+                        if(!isOnNewRow) {
+                            if (countOfSpaces != symbInLastRow) {
+                                System.out.print(" ");
+                                countOfSpaces++;
+                            } else if (countOfSymb != numberOfSymb) {
+                                System.out.print("!");
+                                countOfSymb++;
+                            } else {
+                                System.out.print(" ");
+                            }
+                            if (j == (w-2)) {
+                                if (symbInLastRow + numberOfSymb > w - 2) {
+                                    isOnNewRow = true;
+                                    symbLeft = numberOfSymb - countOfSymb;
+                                }
+                                else{
+                                    numberOfSymb++;
+                                }
+                            }
+                        }
+                        else{
+                            if (countOfSymb != symbLeft){
+                                System.out.print("!");
+                                countOfSymb++;
+                            }
+                            else {
+                                System.out.print(" ");
+                                if (j == (w-2)){
+                                    isOnNewRow = false;
+                                    numberOfSymb++;
+                                }
+                            }
                         }
                     }
                 }
             }
             System.out.print("\n");
+            symbInLastRow = countOfSpaces + countOfSymb;
             countOfSpaces = 0;
             countOfSymb = 0;
         }
