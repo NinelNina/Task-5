5155939e5a65c32d4056989644b5cbb79d766a87
NinelNina <ninkr@list.ru>
2021-10-20 18:21:15 +0300

    Initial commit

---
+++ src/ru/vsu/cs/kravtsova_n_e/task5/Main.java
@@ -0,0 +1,46 @@
+package ru.vsu.cs.kravtsova_n_e.task5;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printPicture(17, 15);
+    }
+
+    public static void printPicture(int h, int w) {
+        int symbInLastRow = 0;
+        int countOfSpaces = 0;
+        int countOfSymb = 0;
+
+        for (int i = 0; i < h; i++) {
+            symbInLastRow = i-1;
+            for (int j = 0; j < w; j++) {
+                if (i == 0 || i == (h - 1)) {
+                    if (j == 0 || j == (w - 1)) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("-");
+                    }
+                } else {
+                    if (j == 0 || j == (w - 1)) {
+                        System.out.print("|");
+                    } else {
+                        if (countOfSpaces != symbInLastRow) {
+                            System.out.print(" ");
+                            countOfSpaces++;
+                        } else if (countOfSymb != i) {
+                            System.out.print("!");
+                            countOfSymb++;
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                }
+            }
+            System.out.print("\n");
+            countOfSpaces = 0;
+            countOfSymb = 0;
+        }
+    }
+}
+
